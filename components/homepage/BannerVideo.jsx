5ecc9151e70dda425fd35fd02990fb56abbd8468
Grady kisibu <kisibugrady3980@gmail.com>
2023-02-14 10:24:18 +0200

    pushing the video banner position

--- components/homepage/BannerVideo.jsx
+++ components/homepage/BannerVideo.jsx
@@ -5,13 +5,13 @@ export default function BannerVideo() {
   return (
     <Box style={{ opacity: "0.8", marginTop: "30px"}}>
       <iframe
-        width="245"
-        height="120"
+        width="255"
+        height="130"
         src="https://www.youtube.com/embed/pYASKtJOyow?autoplay=1&mute=1"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullscreen
       ></iframe>
     </Box>
   );
