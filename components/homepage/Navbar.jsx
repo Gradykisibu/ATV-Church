5b5350b24a8c22cc31b3478be8191bd1bb370c6a
Grady kisibu <kisibugrady3980@gmail.com>
2023-02-14 10:59:37 +0200

    fixing text on navbar to display

--- components/homepage/Navbar.jsx
+++ components/homepage/Navbar.jsx
@@ -51,7 +51,7 @@ useEffect(() => {
         </Link>
 
 
-        <Box sx={{display:"none"}} className={color ? 'Nav Nav-bg' : 'Navbar'}>
+        <Box className={color ? 'Nav Nav-bg' : 'Navbar'}>
           <Link href="/" style={{textDecoration:"none"}}><p>Home</p></Link>
           <Link href="/" style={{textDecoration:"none"}}><p>Events</p></Link>
           <Link href="/" style={{textDecoration:"none"}}><p>About us</p></Link>
